bfd22848253408b98f2865450a37293021e90ebc
parth23571 <pawal.sateesh@gmail.com>
2020-07-14 10:27:12 +0530

    git 10

--- july1407Pro/src/july1407Pro/test11.java
+++ july1407Pro/src/july1407Pro/test11.java
@@ -4,7 +4,7 @@ public class test11 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("in Test");
+System.out.println("in Test - git 10");
 System.out.println("in Test-git11");
 
 	}
